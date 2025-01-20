198970da9d71eeed002e4af8cf90da68b82d9964
Bence27 <Bence2002327@gmail.com>
2025-01-20 22:44:07 +0100

    Squashed commit of the following:
    commit 5c451a1a911a3614bb27e0a9f09b16cabcca0e79
    Author: Bence27 <Bence2002327@gmail.com>
    Date:   Mon Jan 20 22:43:26 2025 +0100
    
        SidebarMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Fragment } from "react";
+import SidebarMenu from "./components/SidebarMenu/SidebarMenu";
+
+const projects = [{ name: null, date: null, description: null, tasks: [] }];
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <Fragment>
+      <SidebarMenu projects={projects}></SidebarMenu>
+    </Fragment>
   );
 }
 
---
+++ src/components/SidebarMenu/SidebarMenu.jsx
@@ -0,0 +1,20 @@
+export default function SidebarMenu({ projects }) {
+  return (
+    <div className="flex-col grid-cols-1 justify-items-center bg-stone-950 w-1/3 mt-8 rounded-tr-md h-screen">
+      <h1 className="text-white text-center text-base md:text-2xl p-2">
+        Your Projects
+      </h1>
+      <button className="mt-5 px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+        + Add Project
+      </button>
+      <div className="mt-8">
+        {projects.length > 0 &&
+          projects.map((project, index) => (
+            <p className="text-white" key={index}>
+              {project.name}
+            </p>
+          ))}
+      </div>
+    </div>
+  );
+}
