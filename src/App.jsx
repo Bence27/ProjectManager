205453510612b8e22cab1c08827c6f7950b03b46
Bence27 <Bence2002327@gmail.com>
2025-01-21 23:04:23 +0100

    Squashed commit of the following:
    commit 736834eb0bac1f6e80b8a291e99c2114de97ea46
    Author: Bence27 <Bence2002327@gmail.com>
    Date:   Tue Jan 21 23:04:11 2025 +0100
    
        SaveLogic

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,33 @@ import SidebarMenu from "./components/SidebarMenu/SidebarMenu";
 import FormContent from "./components/FormContent/FormContent";
 import { useState } from "react";
 
-const PROJECTS = [{ name: null, date: null, description: null, tasks: [] }];
 function App() {
-  const [projects, setProjects] = useState(PROJECTS);
+  const [projects, setProjects] = useState([]);
+  const [isCreateProject, setCreateProject] = useState(false);
+  function addNewProject(name, date, description) {
+    let newProject = [{ name: null, date: null, description: null, tasks: [] }];
+    newProject.name = name;
+    newProject.date = date;
+    newProject.description = description;
+    setProjects([...projects, newProject]);
+  }
+  function handleStateChange() {
+    setCreateProject((isCreateProject) => !isCreateProject);
+  }
+  function handleStateChangeMenu() {
+    setCreateProject(true);
+  }
   return (
     <Fragment>
-      <SidebarMenu projects={projects}></SidebarMenu>
-      <FormContent></FormContent>
+      <SidebarMenu
+        projects={projects}
+        handleStateChangeMenu={handleStateChangeMenu}
+      ></SidebarMenu>
+      <FormContent
+        addNewProject={addNewProject}
+        isCreateProject={isCreateProject}
+        handleStateChange={handleStateChange}
+      ></FormContent>
     </Fragment>
   );
 }
